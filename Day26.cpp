ffd4eb1e8bffb7f5befa31082060d711ac3d82e9
Rahul Gupta <rahul8588838394@gmail.com>
2025-03-29 18:44:15 +0530

    Update Day26.cpp

--- Day26.cpp
+++ Day26.cpp
@@ -1,5 +1,27 @@
-// Question 1
+// Question 1 Doubly linked list Insertion at given position
+//https://bit.ly/3QD4hHs
+class Solution {
+  public:
+    Node *addNode(Node *head, int pos, int data) {
+        Node* t = new Node(data);
+        if (head == NULL) return t;
 
+        Node* temp = head;
+        for (int i = 0; i < pos && temp->next != NULL; i++) {
+            temp = temp->next;
+        }
+
+        t->next = temp->next;
+        t->prev = temp;
+        temp->next = t;
+
+        if (t->next != NULL) {
+            t->next->prev = t;
+        }
+
+        return head;
+    }
+};
 
 // Question 2 Search in Linked List
 //https://bit.ly/3Epriup
